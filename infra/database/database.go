b694511ead06124992173f6995081a874261c261
Mutiakhrnniza2002 <117338908+Mutiakhoirunniza@users.noreply.github.com>
2023-10-23 16:06:08 +0700

    melakukan perubahan ke database

--- infra/database/database.go
+++ infra/database/database.go
@@ -21,18 +21,3 @@ func InitDBMysql(cfg *config.AppConfig) {
     }
 }
 
-// var db *gorm.DB // Objek GORM untuk koneksi database
-
-// func InitDBMysql(cfg *config.AppConfig) *gorm.DB {
-// 	var err error
-// 	// declare struct config & variable connectionString
-// 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-// 		cfg.DBUSER, cfg.DBPASS, cfg.DBHOST, cfg.DBPORT, cfg.DBNAME)
-
-// 	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	return db
-// }
