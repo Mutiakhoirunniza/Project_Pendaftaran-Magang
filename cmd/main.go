bf50a6b9869a17628f9802d66e8aa95dcae0018a
Mutiakhrnniza2002 <117338908+Mutiakhoirunniza@users.noreply.github.com>
2023-10-23 16:04:49 +0700

    melakukan perubahan untuk file main

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"log"
 	"miniproject/delivery/controllers"
 	"miniproject/routes"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	// Load environment variables from the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to load the .env file")
+	}
 
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	
-	// Inisialisasi controllers
-	adminControllers := controllers.NewAdminController(db)
-	internshipControllers := controllers.NewInternshipController(db)
+	// Initialize the database connection
+	dsn := os.Getenv("DBUSER") + ":" + os.Getenv("DBPASS") + "@tcp(" + os.Getenv("DBHOST") + ":" + os.Getenv("DBPORT") + ")/" + os.Getenv("DBNAME") + "?parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to the database")
+	}
 
-	// Set up router
-	e := routes.Initmyrouter(adminControllers, internshipControllers)
-	e.Logger.Fatal(e.Start(":8080"))
+	// Initialize logger
+	logger := log.New(os.Stdout, "MYAPP: ", log.Ldate|log.Ltime)
 
+	// Create instances of your controllers
+	adminController := controllers.NewAdminController(db, logger)
+	userController := controllers.NewUserController(db, logger)
+
+	server := routes.InitmyRoutes(adminController, userController)
+
+	// Start the server on a separate line
+	server.Start(":" + os.Getenv("SERVERPORT"))
 }
