bc3d7c375167f0f6b819e2a0ab68754a53a1e71f
Mutiakhrnniza2002 <117338908+Mutiakhoirunniza@users.noreply.github.com>
2023-10-25 17:43:53 +0700

    penambahan field

--- entity/user.go
+++ entity/user.go
@@ -3,25 +3,19 @@ package entity
 import "gorm.io/gorm"
 
 type User struct {
-	gorm.Model
-	// UserID            int    `gorm:"primaryKey" form:"user_id"`
-	Username          string `json:"username" form:"username" gorm:"unique;not null"`
-	Password          string `json:"password" form:"password" gorm:"not null"`
-	Email             string `json:"email" form:"email" gorm:"unique;not null"`
-	Gender            string `json:"gender" form:"gender"`
-	PhoneNumber       int `json:"phone_number" form:"phone_number"`
-	UniversityName    string `json:"university_name" form:"university_name"`
-	UniversityAddress string `json:"university_address" form:"university_address"`
-	Major             string `json:"major" form:"major"`
-	ProfilePicture    string `json:"profile_picture" form:"profile_picture"`
+    gorm.Model
+    Username           string                    `json:"username" form:"username" gorm:"unique;not null"`
+    Password           string                    `json:"password" form:"password" gorm:"not null"`
+    Email              string                    `json:"email" form:"email" gorm:"unique;not null"`
+    Gender             string                    `json:"gender" form:"gender"`
+    PhoneNumber        string                    `json:"phone_number" form:"phone_number"`
+    UniversityName     string                    `json:"university_name" form:"university_name"`
+    UniversityAddress  string                    `json:"university_address" form:"university_address"`
+    Major              string                    `json:"major" form:"major"`
+    ProfilePicture     string                    `json:"profile_picture" form:"profile_picture"`
 }
 
-
-type UserResponse struct {
-	ID       int    
-	Username string 
-	Password string 
-	Email    string 
-	Token	 string
+type ErrorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
 }
-
